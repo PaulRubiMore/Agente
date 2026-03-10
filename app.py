6bc5373741f949dbc63fe590155d1df20f36a88a
PaulRubiMore <pm5889401@gmail.com>
2026-03-10 07:02:38 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1170,7 +1170,7 @@ def main():
                 df_tecnicos_ot  = tecnicos_por_ot(cron)
                 cron = dividir_especialidades(cron)
                 matriz_tecnicos = optimizar_tecnicos_turnos(cron)
-                st.session_state.update({"cron": cron, "cs": cs, "tecnicos": df_tecnicos, "tecnicos_ot": df_tecnicos_ot, "matriz_tecnicos": matriz_tecnicos})
+                st.session_state.update({"cron": cron, "cs": cs, "tecnicos": df_tecnicos, "tecnicos_ot": df_tecnicos_ot, cron = dividir_especialidades(cron), "matriz_tecnicos": matriz_tecnicos})
             except Exception as e:
                 st.error(f"❌ Error: {e}")
                 st.exception(e)
@@ -1427,6 +1427,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
 
