1c08f88d56db221bbd9db85ff1c626a691a4cd3f
PaulRubiMore <pm5889401@gmail.com>
2026-03-11 02:25:13 -0500

    Update app2.py

--- app2.py
+++ app2.py
@@ -826,7 +826,7 @@ def main():
 
     st.subheader("📊 Gantt por Orden de Trabajo (por horas de técnicos)")
     st.caption("Cada barra = horas trabajadas de una OT por técnico")
-    st.plotly_chart(plot_gantt_ot(matriz_tecnicos), use_container_width=True)
+    st.plotly_chart(plot_gantt_ot_turnos(matriz_tecnicos), use_container_width=True)
 
     
     # ── TABS ──
